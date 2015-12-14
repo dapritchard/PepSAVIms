ce40d6442757055a009a89791822781c27ffdaa1
dpritchLibre <dpritch@live.unc.edu>
2015-12-14 13:34:43 -0500

    Added RStudio project file

--- R/RankLasso.R
+++ R/RankLasso.R
@@ -4,24 +4,40 @@
 #' Returns a vector containing the compounds in the order in which they first
 #' enter the Lasso model
 #'
-#' @param msDat The \emph{mass spectrometry} data
+#' @param ms The mass spectrometry data
 #'
-#' @param actDat The bioactivity data
+#' @param act The bioactivity data
 
-rankLasso <- function(msDat, activity, actAve, region, useAve, useFirst, numRank) {
-  numRep <- ncol(activity)
 
+rankLasso <- function(ms, act, msIden, region, useAve=TRUE, byName=TRUE) {
+  # Transpose of mass spec data; converts to a matrix
+  msDat <- t( ms[, -msIden] )
+  # Transpose of bioactivity data; converts to a matrix
+  actDat <- t(act)
+  # Number of bioactivity replicates
+  numRep <- ncol(actDat)
+
+
+  # Shorten names to fraction numbers
+  substrRight <- function(x, n) {
+    substr(x, nchar(x) - n + 1, nchar(x))
+  }
+  row.names(msDat) <- substrRight(row.names(msDat), 2)
+  row.names(actDat) <- substrRight(row.names(actDat), 2)
+
+  # Indexes the desired region
   regionIdx <- which( row.names(msDat) %in% as.character(region) )
 
   if (useAve) {
     explan <- msDat[regionIdx, ]
+    actAve <- rowMeans(actDat)
     respon <- actAve[regionIdx]
   }
-  else {
-    expanIdx <- regionIdx[rep(1:length(region), each=numRep)]
-    explan <- msDat[expanIdx, ]
-    respon <- c( t(activity[regionIdx, ]) )
-  }
+#   else {
+#     expanIdx <- regionIdx[rep(1:length(region), each=numRep)]
+#     explan <- ms[expanIdx, ]
+#     respon <- c( t(activity[regionIdx, ]) )
+#   }
 
   fit <- lars(x=as.matrix(explan), y=as.matrix(respon))
   actions <- unlist(fit$actions)
@@ -32,10 +48,7 @@ rankLasso <- function(msDat, activity, actAve, region, useAve, useFirst, numRank
   for (k in 1:length(actions)) {
     currTerm <- actions[k]
 
-    if (useFirst)
-      theCond <- ((currTerm > 0) & !(currTerm %in% entryVec))
-    else
-      theCond <- ((currTerm > 0) & !(-currTerm %in% actions[k:actionLen]))
+    theCond <- ((currTerm > 0) & !(currTerm %in% entryVec))
 
     if (theCond) {
       entryVec[ctr] <- currTerm
